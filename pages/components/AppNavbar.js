250b575b51a16e69ba49814a754c2ff1f03d9d11
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-06-18 20:09:03 -0300

    Add Navbar items
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- pages/components/AppNavbar.js
+++ pages/components/AppNavbar.js
@@ -1,14 +1,43 @@
-import { Navbar } from '@mantine/core';
+import { Group, Navbar, Text, ThemeIcon, UnstyledButton } from '@mantine/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComputer, faAtom } from '@fortawesome/free-solid-svg-icons'
 
 export default function AppNavbar({ hidden }) {
+  const links = [
+    {label: 'Item 1', icon: <FontAwesomeIcon icon={faComputer} />},
+    {label: 'Item 2', icon: <FontAwesomeIcon icon={faAtom} />}
+  ]
+
   return (
     <Navbar
-      width={{ sm: 200, lg: 300 }}
+      width={{ sm: 200, lg: 200 }}
       hiddenBreakpoint="sm"
       hidden={hidden}
       p="md"
     >
-      menu aqui
+      {links.map(({ label, icon }) => (
+        <UnstyledButton
+          key={label}
+          sx={(theme) => ({
+            display: 'block',
+              width: '100%',
+              padding: theme.spacing.xs,
+              borderRadius: theme.radius.sm,
+
+              '&:hover': {
+                backgroundColor: theme.colors.gray[0],
+              },
+          })}
+        >
+          <Group>
+            <ThemeIcon color="gray" variant="light">
+              {icon}
+            </ThemeIcon>
+
+            <Text size="sm">{label}</Text>
+          </Group>
+        </UnstyledButton>
+      ))}
     </Navbar>
   )
 }
