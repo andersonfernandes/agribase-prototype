fb25ca82561e0465a67b59e95d088b65c9bd4cc0
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-06-18 12:53:36 -0300

    Add Font Awesome Icons
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 import { Button } from '@mantine/core';
+import { faCarrot } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Home() {
   return (
-    <Link href="/hello" passHref>
-      <Button component="a">Next link button</Button>
+    <Link href="/" passHref>
+      <Button component="a">
+        <FontAwesomeIcon icon={faCarrot} />
+      </Button>
     </Link>
   );
 }
