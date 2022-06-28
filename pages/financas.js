7695112c8448acc8b59cd36cea8f94bc934e422b
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-06-28 20:21:09 -0300

    Update Finances with real data
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- pages/financas.js
+++ pages/financas.js
@@ -2,16 +2,17 @@ import {Table} from "@mantine/core"
 
 export default function Financas() {
   const elements = [
-    {culture: 'Abacaxi', value: 12.5},
-    {culture: 'Banana', value: 13.5},
-    {culture: 'Laranja', value: 14.5},
-    {culture: 'Mamão', value: 15.5},
-    {culture: 'Manga', value: 16.5},
-    {culture: 'Melancia', value: 17.5},
+    {culture: 'Abacaxi', value: 330.0, unit: 100},
+    {culture: 'Banana', value: 30.0, unit: 100},
+    {culture: 'Laranja', value: 25.0, unit: 100},
+    {culture: 'Mamão', value: 37.5, unit: '15Kg'},
+    {culture: 'Manga', value: 65.0, unit: '24Kg'},
+    {culture: 'Melancia', value: 15.0, unit: '15Kg'},
   ];
   const rows = elements.map((element) => (
     <tr key={element.culture}>
       <td>{element.culture}</td>
+      <td>{element.unit}</td>
       <td>{`R$ ${element.value}`}</td>
     </tr>
   ))
@@ -24,11 +25,14 @@ export default function Financas() {
         <thead>
           <tr>
             <th>Cultura</th>
+            <th>Unidade</th>
             <th>Valor médio de mercado</th>
           </tr>
         </thead>
         <tbody>{rows}</tbody>
       </Table>
+
+      <small>Atualizado em: 28 de Junho de 2022</small>
     </>
   )
 }
