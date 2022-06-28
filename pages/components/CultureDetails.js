170d78b371344c9f72554e732e52a32b83e7ba57
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-06-28 16:42:13 -0300

    Improve details modal
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- pages/components/CultureDetails.js
+++ pages/components/CultureDetails.js
@@ -1,17 +1,47 @@
-import {Modal} from '@mantine/core'
+import {Modal, Image, Grid} from '@mantine/core'
 
 export default function CultureDetails({
   title,
   opened,
-  setOpened
+  setOpened,
+  image
 }) {
   return (
     <Modal
       opened={opened}
       onClose={() => setOpened(false)}
       title={title}
+      size="xl"
     >
-      {title}
+      <Grid>
+        <Grid.Col md={6}>
+          <Image
+            src={image}
+            alt={title}
+            width="100%"
+          />
+        </Grid.Col>
+
+        <Grid.Col md={6}>
+          <div>
+            <strong>Descrição em Áudio</strong>
+
+            <audio controls>
+              <source src="./assets/audio/sample.mp3" type="audio/mp3" />
+
+              Your browser does not support the audio element.
+            </audio>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+        </Grid.Col>
+      </Grid>
     </Modal>
   )
 }
--- pages/culturas.js
+++ pages/culturas.js
@@ -36,6 +36,7 @@ export default function Culturas() {
             <CultureDetails
               title={text}
               opened={openedModal[index]}
+              image={image}
               setOpened={(value) => handleModalToggle(value, index)}
             />
 
